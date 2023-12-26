d1d793cb73b750d0f54566b8e3da939b5eb72a97
jentlvdrpe <jentl.vandorpe@student.vives.be>
2023-12-26 15:32:36 +0100

    Argumenten en database changes
    Mogelijk om argumenten mee te geven wanneer we het programma opnoemen. We veranderen het land van de persoon die opgegeven word in de database en printen die opnieuw af.

--- klasse.py
+++ klasse.py
@@ -1,4 +1,4 @@
-class Data:
+class Vriend:
 	def __init__(self, last_name, first_name, country):
 		self.achternaam = last_name.title()
 		self.voornaam = first_name.title()
--- python_opdracht.py
+++ python_opdracht.py
@@ -2,29 +2,43 @@ import sqlite3, sys, csv
 from klasse import Vriend 
 
 # Definieer je drie argumenten
-#script, inputbestand, database, output = sys.argv
+script, achternaam, voornaam, land, database = sys.argv
 
-vriend = Vriend("Kouwenhoven", "Tim", "Belgium")
-
-query = """
-
-"""
+# Maak een klasse aan
+vriend = Vriend(achternaam, voornaam, land)
 
 # Openen van de database
 try:
-	dbconnectie = sqlite3.connect("friends.db")
+	dbconnectie = sqlite3.connect(database)
 	cursor = dbconnectie.cursor()
 except Exception as e:
 	print("Fout bij het openen database: ", e)
 	sys.exit(1)
 
+# Print de originele database
+print("Originele database:")
+cursor.execute("SELECT * FROM friends")
+for friend in cursor:
+	print(friend)
 
+# Pas de database aan
+cursor.execute('UPDATE friends SET country=? WHERE first_name=?', (vriend.land, vriend.voornaam))
+dbconnectie.commit()
+
+# Print de originele database met de aanpassing
+print("\nAangepaste database:")
+cursor.execute("SELECT * FROM friends")
+for friend in cursor:
+	print(friend)
+
+cursor.close()
+dbconnectie.close()
 
 # Inlezen van een csv-bestand
-try:
+'''try:
 	with open('friends.csv', 'w', newline='') as friends_bestand:
 		writer = csv.writer(friends_bestand)
 		writer.writerow(['name', 'age'])
 except Exception as e:
 	print("Fout bij wegschrijven: ", e)
-	sys.exit(1)
+	sys.exit(1)'''
